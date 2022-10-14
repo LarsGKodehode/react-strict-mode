d514401394047d32ec4ef1aab2a808423d36eaf6
LarsGKodehode <lars.gunnar.kodehodet@gmail.com>
2022-10-14 18:07:49 +0200

    custom hook, slightly modified from discussion

---
+++ src/utilities/customHooks/useCategories.js
@@ -0,0 +1,41 @@
+// Libraries
+import {
+  useState,
+  useEffect
+} from 'react';
+
+// Keep all your endpoint stored somwher easly accessible like a Global file
+const ENDPOINTS = {
+  CATEGORIES: "www.api.example.com/" // Your endpoint here
+};
+
+
+export default function useCategories() {
+  const [ isLoading, setIsLoading ] = useState(true)
+  const [ error, setError ] = useState(null)
+  const [ categories, setCategories ] = useState([])
+  
+  async function fetchAndSetCategories() {
+    setIsLoading(true)
+
+    const repsonse = await fetch(ENDPOINTS.CATEGORIES);
+
+    // Uncertain how to properly check for errors
+    setError(repsonse.status);
+
+    const data = await repsonse.json();
+    setCategories(data);
+
+    // Finally
+    setIsLoading(false)
+  };
+
+  useEffect(() => { fetchAndSetCategories() }, []);
+
+  return {
+    isLoading,
+    error,
+    categories,
+    fetchAndSetCategories,
+  };
+};
